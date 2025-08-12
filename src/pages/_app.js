54d7321919acfdaacc96a9f03f7d72cf243bff6d
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-08-12 11:10:38 +0330

    add dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import ThemeProvider from "@/Context/theme-provider";
 export default function App({ Component, pageProps }) {
   return (
-    <div className="px-3 md:px-30 lg:px-50 py-5 font-dana">
+    <div className="px-3 md:px-30 lg:px-50 py-5 font-dana dark:bg-[#242A38]">
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
